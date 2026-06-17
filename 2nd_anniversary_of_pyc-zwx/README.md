db41d234cdc5c46cf1fbfcbfb3569ccbc37b1a68
Alex-McAvoy <931772462@qq.com>
2026-06-17 15:53:30 +0800

    Site updated: 2026-06-17 15:53:26

--- 2nd_anniversary_of_pyc-zwx/README.md
+++
@@ -1,2 +0,0 @@
-# 2nd_anniversary_of_pyc-zwx
-朋友与其对象的两周年纪念页
--- 2nd_anniversary_of_pyc-zwx/js/bgm.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	bootbox.confirm("是否自动播放BGM?", function(result){
-	    if(result) {
-			$(".bgm").attr("autoplay","autoplay");
-		} 
-	});
-});
